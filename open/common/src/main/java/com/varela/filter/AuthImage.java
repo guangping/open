7d3c36e4cbd73f7b111528a53ecc674553e7edb7
guangping <guangping.sony@gmail.com>
2015-11-06 10:07:29 +0800

    随机码

---
+++ open/common/src/main/java/com/varela/filter/AuthImage.java
@@ -0,0 +1,33 @@
+package com.varela.filter;
+
+import com.varela.utils.Constants;
+import com.varela.utils.VerifyCodeUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * Created by lance on 10/19/2015.
+ * 随机码
+ */
+public class AuthImage extends javax.servlet.http.HttpServlet implements javax.servlet.Servlet {
+
+    public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setHeader("Pragma", "No-cache");
+        response.setHeader("Cache-Control", "no-cache");
+        response.setDateHeader("Expires", 0);
+        response.setContentType("image/jpeg");
+
+        //生成随机字串
+        String verifyCode = VerifyCodeUtils.generateVerifyCode(4);
+        //存入会话session
+        HttpSession session = request.getSession(true);
+        session.setAttribute(Constants.RANDOM_CODE, verifyCode.toLowerCase());
+        //生成图片
+        int w = 200, h = 80;
+        VerifyCodeUtils.outputImage(w, h, response.getOutputStream(), verifyCode);
+    }
+}
---
+++ open/common/src/main/java/com/varela/utils/Constants.java
@@ -0,0 +1,31 @@
+package com.varela.utils;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: guangping
+ * Date: 2014-04-24 21:29
+ * To change this template use File | Settings | File Templates.
+ * <p>
+ * 常用字符变量
+ */
+public class Constants {
+    public static final String UTF8 = "UTF-8";
+
+    public static final String GBK = "GBK";
+
+    //默认盐
+    public static final String SALT = "lance";
+
+
+    public static final String JSON = "common/data";
+
+    public static final String AJAX_JSON = "json";
+
+    //随机码
+    public static final String RANDOM_CODE = "randomCode";
+
+    public static final boolean TRUE =true;
+
+    public static final boolean FALSE =false;
+
+}
