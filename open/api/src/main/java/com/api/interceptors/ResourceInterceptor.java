ca3941d6abe1454076e09767844baac6553e55f5
guangping <guangping.sony@gmail.com>
2016-01-25 13:44:53 +0800

    资源部分

---
+++ open/api/src/main/java/com/api/interceptors/ResourceInterceptor.java
@@ -0,0 +1,22 @@
+package com.api.interceptors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by lance on 2016/1/25.
+ */
+public class ResourceInterceptor extends HandlerInterceptorAdapter {
+    private Logger logger = LoggerFactory.getLogger(ResourceInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        request.setAttribute("ctx", request.getContextPath());
+
+        return super.preHandle(request, response, handler);
+    }
+}
