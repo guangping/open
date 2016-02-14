6f4f4b7da21ffa0ac8152b0a12229cab628548ba
guangping <guangping.sony@gmail.com>
2016-02-14 12:05:32 +0800

    整合elasticsearch

---
+++ open/esearch/src/test/java/com/varela/open/esearch/ESearchTest.java
@@ -0,0 +1,12 @@
+package com.varela.open.esearch;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by lance on 2016/2/14.
+ */
+public class ESearchTest {
+
+    @Test
+    public void run(){}
+}
