8040ff73443d99b23132e59699bf2a77d50110c5
guangping <guangping.sony@gmail.com>
2016-02-26 16:16:11 +0800

    调整

--- open/mongodb/src/test/java/com/varela/mongo/test/MongoTest.java
+++ open/mongodb/src/test/java/com/varela/mongo/test/MongoTest.java
@@ -6,8 +6,10 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * Created by lance on 12/9/2015.
@@ -23,8 +25,10 @@ public class MongoTest extends AbstractTestNGSpringContextTests {
     @Test
     public void run(){
         Access access=new Access();
-        access.setAccessId("dddd");
-        access.setSecret("dddd");
+        access.setAccessId(UUID.randomUUID().toString());
+        access.setSecret("123");
+        access.setDateTime(new Date(System.currentTimeMillis()));
+        access.setStatus(1);
         access.setId(1);
         this.mongoTemplate.insert(access);
     }
