3c42da700a6bdbabb63a100344aa3964bf0b3d98
guangping <guangping.sony@gmail.com>
2016-02-26 16:27:26 +0800

    调整

--- open/mongodb/src/test/java/com/varela/mongo/test/MongoTest.java
+++ open/mongodb/src/test/java/com/varela/mongo/test/MongoTest.java
@@ -2,14 +2,12 @@ package com.varela.mongo.test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 /**
  * Created by lance on 12/9/2015.
@@ -21,19 +19,35 @@ public class MongoTest extends AbstractTestNGSpringContextTests {
     private MongoTemplate mongoTemplate;
 
 
-
     @Test
-    public void run(){
-        Access access=new Access();
-        access.setAccessId(UUID.randomUUID().toString());
-        access.setSecret("123");
-        access.setDateTime(new Date(System.currentTimeMillis()));
-        access.setStatus(1);
-        access.setId(1);
-        this.mongoTemplate.insert(access);
+    public void insert() {
+        Access access = null;
+        for (int i = 0; i < 1000; i++) {
+            access = new Access();
+            access.setAccessId(UUID.randomUUID().toString());
+            access.setSecret(String.valueOf(new Random(10000).nextLong()));
+            access.setDateTime(new Date(System.currentTimeMillis()));
+            access.setStatus(1);
+            access.setId((i + 2));
+            this.mongoTemplate.insert(access);
+        }
+
     }
 
+    @Test
+    public void query() {
+        List<Access> list = this.mongoTemplate.findAll(Access.class);
+        System.out.println(list);
+    }
 
+    @Test
+    public void findById() {
+        Access access = this.mongoTemplate.findById("1", Access.class);
+        System.out.println(access);
+
+        Query query = new Query();
+        this.mongoTemplate.find(query, Access.class);
+    }
 
 
 }
