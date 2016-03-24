4d184c3789bd64a87ea4b8116593b33afcaa57dd
lance <guangping.m@51offer.com>
2016-03-24 18:15:28 +0800

    调整

--- open/mp/src/main/resources/scripts/main.sql
+++ open/mp/src/main/resources/scripts/main.sql
@@ -1,12 +1,25 @@
-CREATE TABLE `sys_user` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `user_name` varchar(20) DEFAULT NULL COMMENT '用户名',
-  `email` varchar(30) DEFAULT NULL COMMENT '邮箱',
-  `phone` varchar(11) DEFAULT NULL COMMENT '手机号码',
-  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `modify_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `state` tinyint(4) NOT NULL DEFAULT '1' COMMENT '状态 1 正常 0 禁用',
-  `type` tinyint(4) NOT NULL DEFAULT '1' COMMENT '类型 1 普通用户 2 管理员',
-  `password` varchar(30) NOT NULL COMMENT '密码',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+CREATE TABLE sys_user (
+  id bigint(20) NOT NULL AUTO_INCREMENT,
+  user_name varchar(20) DEFAULT NULL COMMENT '用户名',
+  email varchar(30) DEFAULT NULL COMMENT '邮箱',
+  phone varchar(11) DEFAULT NULL COMMENT '手机号码',
+  create_time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  modify_time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  state tinyint(4) NOT NULL DEFAULT '1' COMMENT '状态 1 正常 0 禁用',
+  type tinyint(4) NOT NULL DEFAULT '1' COMMENT '类型 1 普通用户 2 管理员',
+  password varchar(30) NOT NULL COMMENT '密码',
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE sys_resources (
+  id bigint(20) NOT NULL AUTO_INCREMENT,
+  name varchar(20) DEFAULT NULL COMMENT '名称',
+  url varchar(50) DEFAULT NULL COMMENT 'url',
+  create_time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  modify_time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  parent_id bigint(20) NOT NULL DEFAULT '0' COMMENT '父编码',
+  identify varchar(5) NOT NULL COMMENT '识别码',
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
