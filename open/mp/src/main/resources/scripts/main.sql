79752aa876709a3a243c35d5aa69fd2e7cbd6121
lance <guangping.m@51offer.com>
2016-03-30 16:57:05 +0800

    调整

--- open/mp/src/main/resources/scripts/main.sql
+++ open/mp/src/main/resources/scripts/main.sql
@@ -31,6 +31,17 @@ CREATE TABLE sys_role (
   create_time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   modify_time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE options (
+  id bigint(20) NOT NULL AUTO_INCREMENT,
+  opt_key varchar(20) NOT NULL COMMENT 'key',
+  opt_values varchar(2048) NOT NULL COMMENT '实际值',
+  descriptions varchar(512) DEFAULT NULL COMMENT '说明',
+  opt_title varchar(128) DEFAULT NULL COMMENT '显示值',
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+
 
 
