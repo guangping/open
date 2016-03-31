dafc6182bf697b608b89a2c52dc16905d8aa6f1b
lance <guangping.m@51offer.com>
2016-03-31 09:34:15 +0800

    调整

--- open/redis/src/test/java/CacheTest.java
+++ open/redis/src/test/java/CacheTest.java
@@ -2,7 +2,6 @@ import com.alibaba.fastjson.JSON;
 import com.varela.cache.RedisCache;
 import com.varela.utils.RandomUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
@@ -18,30 +17,35 @@ public class CacheTest extends AbstractTestNGSpringContextTests {
     @Autowired
     private RedisCache redisCache;
 
-    @Autowired
-    private RedisTemplate<String, Object> redisTemplate;
-
     @Test
     public void keys() {
         List<String> list = this.redisCache.keys();
         System.out.println(JSON.toJSONString(list));
 
-        list=this.redisCache.keys("insurance:cargo:type");
+        list = this.redisCache.keys("insurance:cargo:type");
         System.out.println(JSON.toJSONString(list));
     }
 
     @Test
-    public void setList(){
+    public void setList() {
+        for (int i = 0; i < 100; i++) {
+            this.redisCache.set(RandomUtil.getRandomStr(), RandomUtil.getRandomNum());
+        }
 
+    }
+
+    @Test(threadPoolSize = 3,invocationCount = 100)
+    public void get(){
+        System.out.println(this.redisCache.get("aa"));
     }
 
 
     @Test
-    public void push(){
-        String key="city:list";
-        long s=0;
-        for(int i=0;i<200;i++){
-            s=this.redisCache.lPush(key, RandomUtil.getRandomStr());
+    public void push() {
+        String key = "city:list";
+        long s = 0;
+        for (int i = 0; i < 200; i++) {
+            s = this.redisCache.lPush(key, RandomUtil.getRandomStr());
             System.out.println(s);
         }
     }
