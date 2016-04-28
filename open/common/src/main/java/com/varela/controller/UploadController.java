029e7f4463e8e09794738f8818a94d6767c5347a
lance <guangping.m@51offer.com>
2016-04-28 17:46:26 +0800

    调整

--- open/common/src/main/java/com/varela/controller/UploadController.java
+++ open/common/src/main/java/com/varela/controller/UploadController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
@@ -17,24 +18,26 @@ import java.util.Map;
  */
 @Controller
 @RequestMapping(value = "upload", method = RequestMethod.POST)
-public class UploadController extends BaseController {
+public class UploadController  {
 
     @RequestMapping
     @ResponseBody
-    public APIResult upload() {
+    public APIResult upload(HttpServletRequest request) {
         APIResult apiResult = new APIResult();
 
         try {
             Map<String, MultipartFile> filemap = null;
-            if (this.request instanceof MultipartHttpServletRequest) {
+            if (request instanceof MultipartHttpServletRequest) {
                 filemap = ((MultipartHttpServletRequest) request).getFileMap();
-            }
-            for (Map.Entry<String, MultipartFile> entry : filemap.entrySet()) {
-                MultipartFile multipartFile = entry.getValue();
-                InputStream inputStream = multipartFile.getInputStream();
-                //处理上传逻辑
 
+                for (Map.Entry<String, MultipartFile> entry : filemap.entrySet()) {
+                    MultipartFile multipartFile = entry.getValue();
+                    InputStream inputStream = multipartFile.getInputStream();
+                    //处理上传逻辑
+
+                }
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
