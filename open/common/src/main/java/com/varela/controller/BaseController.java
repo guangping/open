7c9af4d4831cc4b90407a061bd38494d20f35eea
lance <guangping.m@51offer.com>
2016-05-10 17:02:04 +0800

    调整

--- open/common/src/main/java/com/varela/controller/BaseController.java
+++ open/common/src/main/java/com/varela/controller/BaseController.java
@@ -1,12 +1,16 @@
 package com.varela.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 /**
  * Created by lance on 10/28/2015.
@@ -14,6 +18,7 @@ import javax.servlet.http.HttpSession;
 public class BaseController {
 
     protected Logger logger = LoggerFactory.getLogger(getClass());
+
 
     protected HttpServletRequest request;
     protected HttpServletResponse response;
@@ -29,5 +34,27 @@ public class BaseController {
         this.session = request.getSession();
 
     }
+
+    /**
+     * 拦截器中返回json数据
+     */
+    protected void writeJson(String json) throws IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().write(json);
+    }
+
+    protected void writeJson(Object obj) throws IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().write(JSONObject.toJSONString(obj, SerializerFeature.WriteMapNullValue, SerializerFeature.WriteNullStringAsEmpty, SerializerFeature.WriteNullBooleanAsFalse));
+    }
+
+    protected String redirect(String url){
+        return "redirect:"+url;
+    }
+
+    protected String forward(String url){
+        return "forward:"+url;
+    }
+
 
 }
