20f324539bec28e686e1b18e105847edebcc3551
lance <guangping.m@51offer.com>
2016-05-10 14:31:40 +0800

    调整

--- open/api/src/main/java/com/varela/api/service/CacheService.java
+++ open/api/src/main/java/com/varela/api/service/CacheService.java
@@ -1,5 +1,7 @@
 package com.varela.api.service;
 
+import java.util.List;
+
 /**
  * Created by lance on 2016/3/18.
  * 定义缓存基础方法
@@ -10,4 +12,6 @@ public interface CacheService<T> {
      * 设置缓存信息
      * */
     void setCache(T arg);
+
+    //void setCache(List<T> list);
 }
