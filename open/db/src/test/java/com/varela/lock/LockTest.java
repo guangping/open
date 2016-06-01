3a2ba5c2fee2598812da78f9a05cc65aaf58c60d
lance <guangping.sony@gmail.com>
2016-06-01 11:09:01 +0800

    调整

---
+++ open/db/src/test/java/com/varela/lock/LockTest.java
@@ -0,0 +1,15 @@
+package com.varela.lock;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by lance on 2016-06-01.
+ */
+public class LockTest {
+
+    private Lock lock = new ReentrantLock();
+
+
+
+}
