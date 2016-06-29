32b89ef2f16d284f832b263a85aa0000e5928f23
lance <guangping.sony@gmail.com>
2016-06-29 17:36:49 +0800

    调整

--- open/common/src/main/java/com/varela/utils/date/DatePattern.java
+++ open/common/src/main/java/com/varela/utils/date/DatePattern.java
@@ -8,9 +8,15 @@ public class DatePattern {
 
     public static final String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
 
+    public static final String YYYY_MM_DD_HH_MM_SS_CN="yyyy年MM月dd日 HH时MM分ss秒";
+
     public static final String YYYY_MM_DD_HH_MM = "yyyy-MM-dd HH:mm";
 
+    public static final String YYYY_MM_DD_HH_MM_CN = "yyyy年MM月dd日 HH时MM分";
+
     public static final String YYYY_MM_DD = "yyyy-MM-dd";
+
+    public static final String YYYY_MM_DD_CN = "yyyy年MM月dd日";
 
     public static final String YYYYMMDD = "yyyyMMdd";
 
